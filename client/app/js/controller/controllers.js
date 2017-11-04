69549313edf0a93d023a943f7dbac1795b55f8e2
Daniel <sirdangd@gmail.com>
2017-11-04 05:57:23 -0400

    fixed controller and html, need to fix css

--- client/app/js/controller/controllers.js
+++ client/app/js/controller/controllers.js
@@ -1,4 +1,58 @@
 demoApp.controller('mainPageCtrl', function ($scope, $window) {
+    $scope.Profile = true;
+    $scope.Goals = false;
+    $scope.Overview = false;
+    $scope.profs=[];
+
+    $scope.profs.push({
+        name: "Bob Smith",
+        f2: "f2",
+        f3: "f3",
+        f4: "f4",
+        imgSrc : "app/images/bob.jpg"
+    });
+
+    $scope.editMode = false;
+
+    $scope.toggleNames = function() {
+        $scope.editMode = !$scope.editMode;
+    }
+
+    $scope.newMember = function () {
+        if ($scope.addMode == false) {
+			$scope.addMode = true;
+		}else {
+			$scope.addMode = false;
+		}
+    }
+
+    $scope.saveNewMember = function () {
+        $scope.addMode = false;
+        $scope.profs.push({
+            name: $scope.newName,
+            f2: $scope.newF2,
+            f3: $scope.newF3,
+            f4: $scope.newF4,
+            imgSrc: $scope.newImg
+        });
+    }
+    
+    $scope.removeSong = function (index) {
+		var spliced = $scope.songs.splice(index,1);
+	}
+
+    $scope.toggleProfTab = function () {
+        $scope.Profile = true;
+    }
+    
+    $scope.toggleGoalTab = function () {
+        $scope.Goals = true;
+    }
+
+    $scope.toggleOvrvTab = function () {
+        $scope.Overview = true;
+    }
+
     $scope.changeTab = function() {
         if ($scope.Profile == true) {
             $scope.Goals = false;
